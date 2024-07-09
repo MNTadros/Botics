d2146085aa8128a109e01a13ba5e5a0f8b84867c
MNTadros <bobnakhla@gmail.com>
2024-07-08 20:58:53 -0400

    Added tooltips for metal detector.

--- src/main/java/empty/botics/item/custom/MetalDetectorItem.java
+++ src/main/java/empty/botics/item/custom/MetalDetectorItem.java
@@ -1,14 +1,21 @@
 package empty.botics.item.custom;
 
+import empty.botics.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Settings settings) {
@@ -50,6 +57,12 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
+        return state.isIn(ModTags.Blocks.METAL_DETECTOR_DETECTABLE_BLOCKS);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.botics.metal_detector.tooltip"));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
---
+++ src/main/java/empty/botics/util/ModTags.java
@@ -0,0 +1,26 @@
+package empty.botics.util;
+
+import empty.botics.Botics;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> METAL_DETECTOR_DETECTABLE_BLOCKS =
+                createTag("metal_detector_detectable_blocks");
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, new Identifier(Botics.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, new Identifier(Botics.MOD_ID, name));
+        }
+    }
+}
